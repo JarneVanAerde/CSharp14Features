7c4ecf7c5a3c829b82c153bd6e48d3626b7f00e5
Jarne Van Aerde <jarnevanaerde@MacBook-Pro-van-Jarne.local>
2025-07-05 19:49:20 +0200

    Added Unbound generics nameof project

---
+++ CSharp14Features.UnboundGenericsNameof/Program.cs
@@ -0,0 +1,5 @@
+﻿TryParse<int> parse = (string text, out int result) => Int32.TryParse(text, out result);
+
+Console.WriteLine($"The only thing of interest here is the {nameof(TryParse<>)} delegate.");
+
+delegate bool TryParse<T>(string text, out T result);
