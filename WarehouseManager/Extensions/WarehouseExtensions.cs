2306e22dcf5e65f850356126749fb429de28267b
Jarne Van Aerde <jarne.vanaerde@katoennatie.com>
2025-10-27 16:33:52 +0100

    Added a new WarehouseExtensions file for the demo

---
+++ WarehouseManager/Extensions/WarehouseExtensions.cs
@@ -0,0 +1,20 @@
+﻿using WarehouseManager.Models;
+
+namespace WarehouseManager.Extensions;
+
+public static class WarehouseExtensions
+{
+    // TODO: convert this method to the new extension method syntax
+    public static void CleanWarehouses(this IEnumerable<Warehouse> warehouses)
+    {
+        foreach (var warehouse in warehouses)
+        {
+            warehouse.Quantity = 0;
+        }
+    }
+
+    // TODO: add an extension property called 'Total' that returns the total quantity of all warehouses
+
+    // TODO: add a static extension property called 'Empty' that returns an empty list of warehouses
+
+}
