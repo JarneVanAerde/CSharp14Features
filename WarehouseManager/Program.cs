092ca7565c8a6e288be8a7a6cac5fc7e4b040853
Jarne Van Aerde <jarne.vanaerde@katoennatie.com>
2025-11-07 07:28:05 +0100

    Included the implicit lamda types feature into the demo

--- WarehouseManager/Program.cs
+++ WarehouseManager/Program.cs
@@ -7,16 +7,22 @@ var options = new DbContextOptionsBuilder<WarehouseContext>()
     .Options;
 
 using var dbContext = new WarehouseContext(options);
-var warehouseService = new WarehouseService(dbContext);
 await WarehouseSeeder.Seed(dbContext);
 
 var warehouseToUpdate = await dbContext.Warehouses.FindAsync(1);
+
+Console.Write("Enter new quantity for Warehouse with Id 1: ");
+var input = Console.ReadLine();
+
 if (warehouseToUpdate != null)
 {
-    warehouseToUpdate.Quantity = 42;
+    if (WarehouseQuantityParser.TryParseQuantity(input!, out var parsedQuantity))
+    {
+        warehouseToUpdate.Quantity = parsedQuantity;
+    }
 }
-dbContext.SaveChanges();
 
+dbContext.SaveChanges();
 
 foreach (var warehouse in dbContext.Warehouses)
 {
---
+++ WarehouseManager/Services/WarehouseQuantityParser.cs
@@ -0,0 +1,18 @@
+﻿namespace WarehouseManager.Services;
+
+public static class WarehouseQuantityParser
+{
+    delegate bool TryParse<T>(string text, out T result);
+
+    public static bool TryParseQuantity(string input, out int quantity)
+    {
+        // TODO: show off the implicit lambda types feature
+        TryParse<int> parse = (string text, out int result) =>
+        {
+            text = text.Trim();
+            return int.TryParse(text, out result);
+        };
+
+        return parse(input, out quantity);
+    }
+}
